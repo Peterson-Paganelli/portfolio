15ffe325fb9a919e3b0b243f9288a49969206bad
Peterson <petersonpaganelli@outlook.com>
2022-12-20 16:21:10 -0300

    Atualiza a página de contato e adiciona localização

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,7 @@ import Header from "../components/Header";
 import Footer from '../components/Footer';
 import '../styles/Contact.css'
 import '../App.css'
-import { FaRegEnvelope, FaLinkedin, FaGithub } from "react-icons/fa";
+import { FaRegEnvelope, FaLinkedin, FaGithub, FaMapMarkedAlt } from "react-icons/fa";
 function Contact() {
   return(
     <div>
@@ -11,6 +11,7 @@ function Contact() {
       <div
       className="contact"
       >
+        
         <h1>Contact</h1>
         <div>
           <div>
@@ -18,10 +19,23 @@ function Contact() {
             <h3>petersonpaganelli@outlook.com</h3>
           </div>
           <div
-          className="icons"
-          >
+          className="icons">
             <a className='linkedinIcon' href="https://br.linkedin.com/in/peterson-paganelli-1832b91b9?trk=profile-badge"><FaLinkedin /></a>
             <a className='githubIcon' href="https://github.com/Peterson-Paganelli"><FaGithub /></a>
+          </div>
+
+          <div className="localization-div">
+            <p className='mapIcon'><FaMapMarkedAlt /></p>
+            <div className="localization">
+              <div>
+                <p>State:</p>
+                <p>City:</p>
+              </div>
+              <div>
+                <p>Santa Catarina</p>
+                <p>Porto Belo</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>

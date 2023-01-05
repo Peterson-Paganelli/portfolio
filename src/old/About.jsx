54eb3a7df5c0e934e27592cc60fe9c7553aa4246
Peterson <petersonpaganelli@outlook.com>
2023-01-04 21:46:29 -0300

    Adiciona um monte de coisa

---
+++ src/old/About.jsx
@@ -0,0 +1,45 @@
+// import React from "react";
+// import Header from "../components/Header";
+// import Footer from "../components/Footer";
+// import '../App.css'
+// import '../styles/About.css'
+
+function About() {
+  return(
+    <div>
+      <div
+      className="title-text">
+        <h1>Sobre mim</h1>
+      </div>
+      <section
+      className="text-div">
+        <p
+        className="text-paragraph">
+          Estudante de desenvolvimento Web pela Trybe.
+          Atualmente estudando Node.js e Express, possuo conhecimentos em Git, 
+          Github, HTML, CSS, Javascript, Jest, React, RTL, Metodologias Ágeis, 
+          Redux, Context API, Docker, mySql, Node.js, Express.
+        </p>
+        <p
+        className="text-paragraph">
+          Comecei a estudar desenvolvimento de software no começo do ano de 2020, 
+          logo após o inicío da pandemia do Covid-19. No final do mesmo ano entrei 
+          na faculdade de Sistemas de informação pela Universidade do Estado de Santa Catarina, 
+          onde estudei até o 3° semestre antes de fazer o processo seletivo para entrar na Trybe.
+        </p>
+        <p
+        className="text-paragraph">
+          Trabalhei um ano na área de Suporte de TI, 
+          prestando atendimento ao usuário, fazendo montagem e manutenção em computadores 
+          e realizando o gerenciamento da rede interna da empresa.
+          Meu objetivo na Trybe é me tornar um desenvolvedor Web com amplo conhecimento em 
+          Front-End, Back-End e Ciência da computação.
+        </p>
+      </section>
+    </div>
+  )
+};
+
+About()
+
+// export default About;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -12,31 +12,32 @@ function About() {
       className="title-text">
         <h1>Sobre mim</h1>
       </div>
-      <section
-      className="text-div">
-        <p
-        className="text-paragraph">
-          Estudante de desenvolvimento Web pela Trybe.
-          Atualmente estudando Node.js e Express, possuo conhecimentos em Git, 
-          Github, HTML, CSS, Javascript, Jest, React, RTL, Metodologias Ágeis, 
-          Redux, Context API, Docker, mySql, Node.js, Express.
-        </p>
-        <p
-        className="text-paragraph">
-          Comecei a estudar desenvolvimento de software no começo do ano de 2020, 
-          logo após o inicío da pandemia do Covid-19. No final do mesmo ano entrei 
-          na faculdade de Sistemas de informação pela Universidade do Estado de Santa Catarina, 
-          onde estudei até o 3° semestre antes de fazer o processo seletivo para entrar na Trybe.
-        </p>
-        <p
-        className="text-paragraph">
-          Trabalhei um ano na área de Suporte de TI, 
-          prestando atendimento ao usuário, fazendo montagem e manutenção em computadores 
-          e realizando o gerenciamento da rede interna da empresa.
-          Meu objetivo na Trybe é me tornar um desenvolvedor Web com amplo conhecimento em 
-          Front-End, Back-End e Computer Science.
-        </p>
-      </section>
+      <div className="sections-div">
+
+        <section className="text-div">
+          <h1 className="sub-title">Tecnologias</h1>
+          <div className="techs-div">
+            <ul>
+              <li>React.js</li>
+              <li>Node.js</li>
+              <li>MySql</li>
+            </ul>
+          </div>
+        </section>
+
+        <section className="text-div">
+          <h1 className="sub-title">Trabalhos anteriores</h1>
+          <div className="jobs">
+            <h3>Artefama</h3>
+            <h3>Suporte em TI</h3>
+          </div>
+          <div className="jobs">
+            <h3>Artefama</h3>
+            <h3>Estágiario em TI</h3>
+          </div>
+        </section>
+
+      </div>
       <Footer />
     </div>
   )
